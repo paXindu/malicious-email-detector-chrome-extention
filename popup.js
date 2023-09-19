dd542b6fdab34fb2720b9f561d6e6c0615549457
paXindu <62470295+paXindu@users.noreply.github.com>
2023-09-19 18:09:46 +0530

    project completed

--- popup.js
+++ popup.js
@@ -9,8 +9,51 @@ function scanEmail() {
   });
 }
 
+function readURLs() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      files: ["readURLScript.js"],
+    }, function (result) {
+      const urlList = result[0].result;
+      console.log(urlList);
+
+      if (urlList.length === 0) {
+        document.getElementById("serverResponse").innerText = "No URLs to process.";
+        return;
+      }
+
+      // Send the urlList to the server
+      fetch('http://127.0.0.1:5000/predict/phishing', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ urls: urlList })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          const goodCount = data.predictions.filter(prediction => prediction === 'good').length;
+          const total = data.predictions.length;
+          const emailSafetyPercentage = (goodCount / total) * 100;
+
+          if (isNaN(emailSafetyPercentage)) {
+            document.getElementById("serverResponse").innerText = "No URLs to process.";
+          } else {
+            document.getElementById("serverResponse").innerText = `Email Safety Percentage: ${emailSafetyPercentage.toFixed(2)}%`;
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("scanEmailButton").addEventListener("click", scanEmail);
+  document.getElementById("readURLButton").addEventListener("click", readURLs);
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -23,7 +66,10 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     })
       .then(response => response.json())
       .then(data => {
-        document.getElementById("output").innerText = JSON.stringify(data);
+        const prediction = data.prediction;
+        const predictionPercentage = data.prediction_percentage;
+
+        document.getElementById("output").innerText = `Prediction: ${prediction}\nPrediction Percentage: ${predictionPercentage}%`;
       })
       .catch(error => {
         console.error('Error:', error);
